f481a1648a48899a65d5155641e4b6e66be021f0
pankhilprajapati <pankhil.prajapati2017@vitstudent.ac.in>
2019-05-10 20:00:55 +0530

    basic of file upload(images)

--- src/index.js
+++ src/index.js
@@ -7,17 +7,14 @@ const taskRouter = require('./routes/task')
 const app = express()
 const port = process.env.PORT || 3000
 
-// app.use((req, res, next)=>{
-//     if(req.method === 'GET'){
-//         res.send("Ge request are disable")
-//     }else{
-//         next()
-//     }
-// })
+const multer = require("multer")
+const upload = multer({
+    dest: 'images'
+})
 
-// app.use((req, res, next)=>{
-//     res.status(503).send("Site is at maintenance")
-// })
+app.post("/upload", upload.single('upload'), (req,res)=>{
+    res.send()
+})
 
 app.use(express.json())
 app.use(userRouter)
@@ -29,13 +26,3 @@ app.listen(port, () => {
     console.log('Server is up on port ' + port)
 })
 
-const Task = require("./models/task"),
-      User = require("./models/user")
-
-const main = async ()=>{
-    const user = await User.findById('5cd549daf598566cdaef2981')
-    await user.populate('tasks').execPopulate()
-    console.log(user.tasks)
-}
-
-main()

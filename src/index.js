d8b41dc3a7c48410fa81051f038a871fc215fa57
pankhilprajapati <pankhil.prajapati2017@vitstudent.ac.in>
2019-05-10 13:23:19 +0530

    cleaning index

--- src/index.js
+++ src/index.js
@@ -29,13 +29,3 @@ app.listen(port, () => {
     console.log('Server is up on port ' + port)
 })
 
-const jwt = require('jsonwebtoken')
-const myFunc = async()=>{
-   const token = jwt.sign({ _id: 'abc123' },"thisismynewcourse",{ expiresIn: '7 days' })
-   console.log(token)
-
-   const data = jwt.verify(token, 'thisismynewcourse')
-   console.log(data)
-}
-
-myFunc()

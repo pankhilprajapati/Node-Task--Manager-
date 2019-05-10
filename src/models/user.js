f7e992ed3ffc93a10714c83c50501e155e1be3ec
pankhilprajapati <pankhil.prajapati2017@vitstudent.ac.in>
2019-05-10 15:01:14 +0530

    Some auth changes end points

--- src/models/user.js
+++ src/models/user.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose')
 const validator = require('validator')
-const bcrypt = require("bcryptjs")
-const jwt = require("jsonwebtoken")
+const bcrypt = require('bcryptjs')
+const jwt = require('jsonwebtoken')
 
 const userSchema = new mongoose.Schema({
     name: {
@@ -41,16 +41,16 @@ const userSchema = new mongoose.Schema({
             }
         }
     },
-    tokens:[{
-        token : {
-            type :String,
+    tokens: [{
+        token: {
+            type: String,
             required: true
         }
     }]
 })
 
-userSchema.methods.toJSON = function (){
-    const user = this 
+userSchema.methods.toJSON = function () {
+    const user = this
     const userObject = user.toObject()
 
     delete userObject.password
@@ -69,25 +69,27 @@ userSchema.methods.generateAuthToken = async function () {
     return token
 }
 
-userSchema.statics.findByCredentials = async (email, password) =>{
-    const user = await User.findOne({email})
+userSchema.statics.findByCredentials = async (email, password) => {
+    const user = await User.findOne({ email })
 
-    if(!user){
-        throw new Error("unable to login")
+    if (!user) {
+        throw new Error('Unable to login')
     }
 
     const isMatch = await bcrypt.compare(password, user.password)
-    if(!isMatch){
-        throw new Error('unable to login')
+
+    if (!isMatch) {
+        throw new Error('Unable to login')
     }
+
     return user
 }
 
-// hash plain
-userSchema.pre('save', async function(next){
+// Hash the plain text password before saving
+userSchema.pre('save', async function (next) {
     const user = this
 
-    if(user.isModified('password')){
+    if (user.isModified('password')) {
         user.password = await bcrypt.hash(user.password, 8)
     }
 
